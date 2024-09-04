a1cd3d53fb44305174724e642f4727dd67bb4160
joseDuc <ppgq@live.com>
2024-09-04 13:28:43 +0200

    first commit

---
+++ src/main/java/com/lab_java_intro_to_spring_boot/LabJavaIntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lab_java_intro_to_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaIntroToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJavaIntroToSpringBootApplication.class, args);
+	}
+
+}
